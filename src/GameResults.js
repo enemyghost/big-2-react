935f0e553b0f22b5cd8e8290ada29876cbef777b
Tyler Edelen <tedelen@conductor.com>
2018-10-30 19:50:23 -0400

    add winner final play

--- src/GameResults.js
+++ src/GameResults.js
@@ -7,9 +7,13 @@ class GameResults extends Component {
       return a.cards.length - b.cards.length;
     }).map((handView, idx) => {
       let winner = handView.cards.length === 0;
+      let hand = winner ? this.props.finalState.lastPlays[0].hand : handView.cards;
+      if (winner) {
+        hand.forEach((card) => card.selected = true);
+      }
       return (<div key={handView.player.name + idx}>
         <h1>{handView.player.name + (winner ? " wins." : "")}</h1>
-        <PlayerHand cards={handView.cards} onSelected={(card, e) => { }}/>
+        <PlayerHand cards={hand} onSelected={(card, e) => { }}/>
       </div>)
     });
     return (<div className="resultsPanel">
