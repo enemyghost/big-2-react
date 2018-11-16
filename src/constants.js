145d0cf7292bf7153534db518954bf4a7bd35d6f
Tyler Edelen <tedelen@conductor.com>
2018-11-15 22:08:43 -0500

    prod config

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// const hostname = "https://arcane-forest-67352.herokuapp.com/v1";
-const hostname = "http://localhost:8080/v1";
+const hostname = "https://arcane-forest-67352.herokuapp.com/v1";
+// const hostname = "http://localhost:8080/v1";
 const TOKEN_LOCALSTORAGE_NAME = 'com.gmo.big2.jwt.token';
 const TOKEN_EXPIRATION_KEY = 'exp';
 
